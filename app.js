a525e9204bc055e6aa74990d5a11b3aa9f7a69ef
OlgaSivyuk <olchik0308@yandex.ru>
2022-06-19 23:26:50 +0400

    test errors

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+// обработка несуществующего роута
+app.use((req, res) => {
+  res.status(404).send({ message: 'Страницы не существует' });
+});
+
 app.listen(PORT, () => {
   console.log('App started and listen port', PORT);
 });

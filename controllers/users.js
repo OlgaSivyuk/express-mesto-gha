595a8ab0d25f84e8ca11bc731942ab165f384839
OlgaSivyuk <olchik0308@yandex.ru>
2022-06-27 18:21:44 +0400

    feat: add jwt validation

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const SALT_ROUNDS = 10;
+const SECRET_KEY = 'very_secret';
 // const OK_CODE = 200;
 // const BAD_REQ_ERROR_CODE = 400;
 // const NOT_FOUND_ERROR_CODE = 404;
@@ -49,7 +51,7 @@ module.exports.createUser = (req, res) => {
     })
     .then((user) => {
       // console.log(user);
-      res.status(201).send({ data: user, message: 'пользователь создан' });
+      res.status(201).send({ data: user, message: 'пользователь создан' }); // исключить передачу пароля, раскрыть data - name: user.name
       // res.send({ message: 'пользователь создан' });
     })
     .catch((err) => {
@@ -134,9 +136,53 @@ module.exports.updateAvatar = (req, res) => {
     });
 };
 
-module.exports.login = (req) => {
-  // const { email, password } = req.body;
-  console.log(req, 'auth_login');
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  // console.log(req, 'auth_login');
+
+  if (!email || !password) {
+    return res.status(400).send({ message: 'не передан email или пароль' });
+  }
+
+  return User.findOne({ email }) // способ №1 провалидировать данные нового юзера поискать по имейлу
+    .then((foundUser) => {
+      console.log(foundUser);
+      // return res.status(409).send({ message: 'email занят' });
+      if (!foundUser) {
+        const err = new Error({ message: 'не верный email или пароль' });
+        err.statusCode = 403;
+        throw err;
+        // return res.status(403).send({ message: 'не верный email или пароль' });
+      }
+      return Promise.all([
+        foundUser,
+        bcrypt.compare(password, foundUser.password),
+      ]);
+      // return bcrypt.compare(password, user.password);
+    })
+    .then(([user, isPasswordCorrect]) => { // проверяем корректность пароля при логине
+      // console.log('in second then');
+      if (!isPasswordCorrect) {
+        const err = new Error({ message: 'не верный email или пароль' });
+        err.statusCode = 403;
+        throw err;
+        // return res.status(403).send({ message: 'не верный email или пароль' });
+      }
+      // console.log('password');
+      // если пароль совпал с созданным,  возвращаем токен
+      // return res.send({ message: 'здесь нужно вернуть токен' });
+      return jwt.sign({ email: user.email }, SECRET_KEY); // работает ассинхронно
+    })
+    .then((token) => {
+      console.log(token);
+      res.send({ token });
+    })
+    .catch((err) => {
+      if (err.statusCode === 403) {
+        return res.status(403).send({ message: 'тут месседж об ошибке' });
+      }
+      return res.status(DEFAULT_ERROR_CODE).send({ message: 'Ошибка на сервере' });
+    });
 };
 
 // module.exports.createUser = (req, res) => {

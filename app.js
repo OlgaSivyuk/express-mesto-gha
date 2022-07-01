5c4e7d1eed6aa37815669fe0ff02119d15073177
OlgaSivyuk <olchik0308@yandex.ru>
2022-07-01 19:16:55 +0400

    auth test

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+// const celebrate = require('celebrate');
+
 const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,8 +29,12 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    select: false, // необходимо добавить поле select
-    // validator:
+    select: false,
+    validator: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+    },
   },
 });
 

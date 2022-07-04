ea0060c46475bfcb042618f0dcc3a99313f99c68
OlgaSivyuk <olchik0308@yandex.ru>
2022-07-04 15:53:28 +0400

    problem with userCreate

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const auth = require('./middlewares/auth');
 
 // const { errors } = require('celebrate');
 // const { celebrate, Joi } = require('celebrate');
-// const { createUser, login } = require('./controllers/users');
+const { createUser, login } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -29,8 +29,8 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-// app.post('/signin', login);
-// app.post('/signup', createUser);
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 // обработка несуществующего роута
 app.use((req, res) => {

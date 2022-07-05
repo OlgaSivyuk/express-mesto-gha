ab9da074afc1d6d6463364a8db6e22bb6f898287
OlgaSivyuk <olchik0308@yandex.ru>
2022-07-05 14:15:16 +0400

    feat: joy validation user

--- routes/users.js
+++ routes/users.js
@@ -18,8 +18,8 @@ router.patch(
   '/me',
   celebrate({
     params: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
     }),
   }),
   updateProfile,
